15999b313eb5169bd505c19e184874f5a71f2c67
Illia <yarfuo@protonmail.com>
2019-07-08 19:48:20 +0300

    Code style

--- bot/callbacks/start.py
+++ bot/callbacks/start.py
@@ -3,6 +3,7 @@ from telegram.ext import CallbackContext
 
 
 def handle_start(update: Update, context: CallbackContext):
+    del context
     update.effective_chat.send_message(
         "Здравствуй! Я помогу распознать QR коды и другие баркоды.\n\n"
         "*Для распознования отправьте мне фотографию.*",
