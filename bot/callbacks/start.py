5bdc319e8749178d0ba2c2b07c1849db5e12e68f
Illia <yarfuo@protonmail.com>
2019-07-08 19:48:25 +0300

    Text change

--- bot/callbacks/start.py
+++ bot/callbacks/start.py
@@ -5,7 +5,7 @@ from telegram.ext import CallbackContext
 def handle_start(update: Update, context: CallbackContext):
     del context
     update.effective_chat.send_message(
-        "Здравствуй! Я помогу распознать QR коды и другие баркоды.\n\n"
+        "Здравствуй! Я помогу распознать QR и другие баркоды.\n\n"
         "*Для распознования отправьте мне фотографию.*",
         parse_mode=ParseMode.MARKDOWN,
     )
