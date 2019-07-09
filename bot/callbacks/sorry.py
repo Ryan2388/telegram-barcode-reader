8ca015c22b1dd66c2da064f63032d8d8cefe417d
Illia <yarfuo@protonmail.com>
2019-07-09 16:25:19 +0300

    Sorry message fix

--- bot/callbacks/sorry.py
+++ bot/callbacks/sorry.py
@@ -4,6 +4,6 @@ from telegram.ext import CallbackContext
 
 def handle_unknown(update: Update, context: CallbackContext):
     del context  # Not used
-    update.effective_chat.reply_text(
+    update.effective_message.reply_text(
         "Извини, я тебя не понимаю. Попробуй команду /start"
     )
